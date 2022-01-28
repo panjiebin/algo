540e3d14e65941cd0925503b62e698072a3ec149
panjb <jiebin.pan@quantil.com>
2022-01-28 11:01:38 +0800

    添加实现队列代码

---
+++ src/main/java/com/code/algo/queue/QueueBaseArray.java
@@ -0,0 +1,65 @@
+package com.code.algo.queue;
+
+/**
+ * 基于数组实现队列
+ * <p>使用两个下标head,tail分别表示队头和队尾。
+ * 入队，tail加1，当tail==capacity且head==0时，表示队列已满；当tail==capacity且head!=0时，需要将数组数据集体左移
+ * 出队，直接返回head对应的元素，head+1，当head==tail时，表示队列已空
+ * <p>出队入队时间复杂度：O(1)</p>
+ * @author panjb
+ */
+public class QueueBaseArray<E> {
+
+    private final Object[] elements;
+    private final int capacity;
+    private int head;
+    private int tail;
+
+    public QueueBaseArray(int capacity) {
+        this.capacity = capacity;
+        this.elements = new Object[capacity];
+        head = 0;
+        tail = 0;
+    }
+
+    public static void main(String[] args) {
+        QueueBaseArray<String> queue = new QueueBaseArray<>(3);
+        queue.enqueue("a");
+        queue.enqueue("b");
+        queue.enqueue("c");
+        System.out.println(queue.dequeue());
+        boolean d = queue.enqueue("d");
+        System.out.println("d = " + d);
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+    }
+
+    public boolean enqueue(E element) {
+        if (tail == capacity) {
+            //对头在初始位置，表示队列已满
+            if (head == 0) {
+                return false;
+            }
+            //将head~tail之间数据集体左移
+            for (int i = head; i < tail; i++) {
+                this.elements[i - head] = this.elements[i];
+            }
+            tail -= head;
+            head = 0;
+        }
+        this.elements[tail] = element;
+        tail++;
+        return true;
+    }
+
+    public E dequeue() {
+        if (head == tail) {
+            return null;
+        }
+        Object element = this.elements[head];
+        head++;
+        return (E) element;
+    }
+}
---
+++ src/main/java/com/code/algo/queue/QueueBaseLinkedList.java
@@ -0,0 +1,59 @@
+package com.code.algo.queue;
+
+/**
+ * 基于链表实现队列
+ * <p> 用指针head,tail分别表示队头和队尾结点，入队：tail.next = new_node,tail=tail.next；
+ * 出队：head = head.next
+ * <p>出队和入队的时间复杂度：O(1)
+ * @author panjb
+ */
+public class QueueBaseLinkedList<E> {
+
+    private Node<E> head;
+    private Node<E> tail;
+
+    public static void main(String[] args) {
+        QueueBaseLinkedList<String> queue = new QueueBaseLinkedList<>();
+        queue.enqueue("a");
+        queue.enqueue("b");
+        queue.enqueue("c");
+        System.out.println(queue.dequeue());
+        queue.enqueue("d");
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+    }
+
+    public void enqueue(E element) {
+        Node<E> node = new Node<>(element);
+        if (head == null) {
+            head = node;
+            tail = node;
+        } else {
+            tail.next = node;
+            tail = tail.next;
+        }
+    }
+
+    public E dequeue() {
+        if (head == null) {
+            return null;
+        }
+        Node<E> node = head;
+        head = head.next;
+        return node.val;
+    }
+
+    private static class Node<E> {
+        private E val;
+        private Node<E> next;
+
+        public Node() {
+        }
+
+        public Node(E val) {
+            this.val = val;
+        }
+    }
+}
